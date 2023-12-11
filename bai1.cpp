7f9f24f11537299d81f043b614e4269dc25766ed
BaoLam2809 <152182435+BaoLam2809@users.noreply.github.com>
2023-12-11 20:40:59 +0700

    Update bai1.cpp

--- bai1.cpp
+++ bai1.cpp
@@ -1,9 +1,10 @@
 // Bài 1: Tìm mối liên hệ
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 
 int main(){
-  ll n;
+  	ll n;
 	cin >> n;
 	ll ans =  n * (n - 1);
 	cout << ans << endl;
